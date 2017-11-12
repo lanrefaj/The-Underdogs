ab6db6d37661874b6182df88dd8df8c409bd0bb2
Lanre Fajimolu <lanre.fajimolu.com>
2017-11-12 12:28:11 -0600

    Merging files into Beta to do some work

--- assets/webstorefront.js
+++ assets/webstorefront.js
@@ -2,4 +2,10 @@ $( document ).ready(function() {
 
 	console.log( "ready!" );
 
+	for (var i = 0; i < items; i++) {
+    	var newdiv = $('<div>')
+    	newdiv.text(items[i])
+    	$('#test').append(newdiv)
+    }
+
 });
